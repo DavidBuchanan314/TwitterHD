0d72211ca06d9ae41e56d975721fb07248b4e0d7
mint <60362123+enbyautumn@users.noreply.github.com>
2022-11-05 16:41:47 -0400

    Add Userscript installation method (#1)
    * Create twitterhd.user.js
    
    * Update README.md

---
+++ twitterhd.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name        TwitterHD - twitter.com
+// @namespace   https://github.com/DavidBuchanan314/TwitterHD
+// @match       *://*.twitter.com/*
+// @grant       none
+// @version     1.0
+// @author      DavidBuchanan314, made into script by mintexists
+// @description Always load Twitter images in maximum available resolution - https://github.com/DavidBuchanan314/TwitterHD
+// @require https://raw.githubusercontent.com/DavidBuchanan314/TwitterHD/main/twitterhd.js
+// ==/UserScript==
